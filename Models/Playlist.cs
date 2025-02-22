4c4fe3882f5de5711e64c41c329efde296c5df59
FelipeMGR <felipematheusgr18@gmail.com>
2025-02-21 22:49:04 -0300

    Small changes to some properties names

--- Models/Playlist.cs
+++ Models/Playlist.cs
@@ -6,6 +6,6 @@ public class Playlist
 {
   public string? Name { get; set; }
   public string? Description { get; set; }
-  public bool IsPublic { get; set; }
+  public bool? IsPublic { get; set; }
   public List<Music>? Musics { get; set; }
 }
