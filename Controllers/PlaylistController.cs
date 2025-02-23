1d68b79bec942f3e1529209257e5b8b036ae6829
FelipeMGR <felipematheusgr18@gmail.com>
2025-02-22 23:30:05 -0300

    General acess logic moved from the controller

--- Controllers/PlaylistController.cs
+++ Controllers/PlaylistController.cs
@@ -1,70 +1,45 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MisticFy.Models;
+using MisticFy.Repositories;
 using SpotifyAPI.Web;
 
 namespace MisticFy.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class PlaylistController : ControllerBase
+    public class PlaylistController(IPlaylistRepository playlist) : ControllerBase
     {
+
+        private readonly IPlaylistRepository _playlist = playlist;
+
         [HttpGet("{playlistId}")]
-        public async Task<ActionResult> GetUserPlaylistAsyc([FromHeader(Name = "Authentication")] string token, [FromRoute] string playlistId)
+        public async Task<ActionResult> GetPlaylistAsyc([FromHeader(Name = "Authentication")] string token, [FromRoute] string playlistId)
         {
-
-            var acessToken = token.Replace("Bearer ", "").Trim();
-
-            if (string.IsNullOrEmpty(playlistId))
+            if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(playlistId))
             {
-                return BadRequest("Playlist ID is required.");
+                return BadRequest("Token/Playlist is required");
             }
 
-            var config = SpotifyClientConfig.CreateDefault().WithToken(acessToken);
-            var client = new SpotifyClient(config);
-
-            var playlist = await client.Playlists.Get(playlistId);
-
-            List<Music> playlistMusics = new List<Music>();
-
-            foreach (var item in playlist.Tracks.Items)
-            {
-                var track = item.Track as FullTrack;
-                if (track != null)
-                {
-                    playlistMusics.Add(new Music
-                    {
-                        MusicName = track.Name
-                    });
-                }
-            }
-            return Ok(new Playlist
-            {
-                Name = playlist.Name,
-                Description = playlist.Description,
-                IsPublic = playlist.Public,
-                Musics = playlistMusics
-            });
+            var userPlaylist = await _playlist.GetUserPlaylistAsync(token, playlistId);
+            return Ok(userPlaylist);
         }
 
         [HttpPost("createPlaylist")]
         public async Task<ActionResult> CreatePlaylistAsync([FromBody] Playlist playlist, [FromHeader(Name = "Authentication")] string token)
         {
-            var acessToken = token.Replace("Bearer ", "");
-
-            var config = SpotifyClientConfig.CreateDefault();
-            var spotify = new SpotifyClient(config.WithToken(acessToken));
-
-            var currentUser = await spotify.UserProfile.Current();
-            string userId = currentUser.Id;
-
-            var newPlayList = await spotify.Playlists.Create(userId, new PlaylistCreateRequest(playlist.Name)
+            if (string.IsNullOrEmpty(token))
             {
-                Description = playlist.Description,
-                Public = playlist.IsPublic
-            });
+                return BadRequest("Authentication token is required");
+            }
+            else if (playlist == null)
+            {
+                return BadRequest("Please, enter the playlist info");
+            }
 
-            return Ok(newPlayList.Name);
+            var userPlaylist = await _playlist.CreatePlaylistAsync(token, playlist);
+
+            return Ok(userPlaylist);
         }
     }
 }
