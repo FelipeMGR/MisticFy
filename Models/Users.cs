9ed61e65ee18d83fada4e2cea6565206a66c55e2
FelipeMGR <felipematheusgr18@gmail.com>
2025-02-28 18:06:42 -0300

    New properties added

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,11 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace MisticFy.Models;
 
 public class Users
 {
-  public string? UserName { get; set; }
-  public string? Password { get; set; }
+  public int Id { get; set; }
+  public string? SpotifyUserId { get; set; }
+  public string? Name { get; set; }
+  public string? Email { get; set; }
+  [Required]
+  public string AccessToken { get; set; }
+  public string RefreshToken { get; set; }
+  public DateTime TokenExpiresAt { get; set; }
   public List<Playlist>? Playlists { get; set; }
   public List<string>? LikedMusics { get; set; }
 }
