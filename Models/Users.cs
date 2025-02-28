0df9c89210d6cf0e988d36a76113e48b40bc275b
FelipeMGR <felipematheusgr18@gmail.com>
2025-02-27 22:39:17 -0300

    New properties added

--- Models/Users.cs
+++ Models/Users.cs
@@ -4,6 +4,8 @@ namespace MisticFy.Models;
 
 public class Users
 {
+  public string? UserName { get; set; }
+  public string? Password { get; set; }
   public List<Playlist>? Playlists { get; set; }
   public List<string>? LikedMusics { get; set; }
 }
